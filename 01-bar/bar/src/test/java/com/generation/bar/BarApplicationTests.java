ee8810830f2c65640477093c06e8620c5557066a
Luca Maita <100094165+lucamaita@users.noreply.github.com>
2024-02-05 11:10:22 +0100

    create project 01-bar

---
+++ 01-bar/bar/src/test/java/com/generation/bar/BarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.bar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
